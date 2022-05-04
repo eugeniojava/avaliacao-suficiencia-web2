2cc43b4a1cecf3fd9afb03aeb9393cc2b5584dfa
Eugênio Moreira <eugeniojava@outlook.com>
2022-05-03 22:49:34 -0300

    feat(client): add search feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+import CreatePost from "./components/CreatePost";
+import Private from "./private";
+
+import { useAuth } from "./contexts/authentication";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  const { currentUser, logout } = useAuth();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? 'Loading...' : data}</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container">
+            <Link
+              className="navbar-brand"
+              to={currentUser ? "posts" : "/login"}
+            >
+              My Posts
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <div className="navbar-nav container">
+                {currentUser ? (
+                  <div className="nav-item">
+                    <Link className="nav-link" to={"/create-post"}>
+                      Create a post
+                    </Link>
+                  </div>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to={"/login"}>
+                        Login
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to={"/register"}>
+                        Register
+                      </Link>
+                    </li>
+                  </>
+                )}
+                <div className="nav-item">
+                  <Link className="nav-link" to={"/posts"}>
+                    Posts
+                  </Link>
+                </div>
+
+                {currentUser && (
+                  <div className="nav-item">
+                    <button
+                      onClick={logout}
+                      className="btn btn-danger ml-auto"
+                      style={{
+                        marginLeft: "auto",
+                      }}
+                    >
+                      Sair
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/post" element={<Post />} />
+
+          <Route
+            path="/create-post"
+            element={
+              <Private>
+                <CreatePost />
+              </Private>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
