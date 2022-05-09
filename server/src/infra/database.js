c65961d89562a26cacd01e1c8c361497851e9b60
Eugênio Moreira <eugeniojava@outlook.com>
2022-05-09 13:45:15 -0300

    fix(server): adjust database config

--- server/src/infra/database.js
+++ server/src/infra/database.js
@@ -4,7 +4,10 @@ const database = pgPromise({
   connectionString:
     process.env.DATABASE_URL ||
     `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
-  ssl: { rejectUnauthorized: false },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : null,
 });
 
 module.exports = database;
