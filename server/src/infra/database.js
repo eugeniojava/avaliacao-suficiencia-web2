872cbc029b0999ebf85b69db1ddbf6847b9343ec
Eugênio Moreira <eugeniojava@outlook.com>
2022-05-09 12:36:58 -0300

    fix(server): set SSL flag to false

--- server/src/infra/database.js
+++ server/src/infra/database.js
@@ -6,7 +6,7 @@ const database = pgPromise({
   database: process.env.DATABASE_NAME,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  ssl: true,
+  ssl: false,
 });
 
 module.exports = database;
